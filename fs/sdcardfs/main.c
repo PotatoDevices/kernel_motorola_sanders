24e819ad2e98ddabb0a1898f778e24b8638493bd
Daniel Rosenberg <drosen@google.com>
2020-06-27 15:19:07 +0200

    ANDROID: sdcardfs: Add option to drop unused dentries
    This is propagated from android 3.18 kernel.
    Mot-CRs-fixed: (CR)
    
    This adds the nocache mount option, which will cause sdcardfs to always
    drop dentries that are not in use, preventing cached entries from
    holding on to lower dentries, which could  cause strange behavior when
    bypassing the sdcardfs layer and directly changing the lower fs.
    
    Change-Id: I70268584a20b989ae8cfdd278a2e4fa1605217fb
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Signed-off-by: Kanagarathina Kalathi <kalathik@google.com>
    Reviewed-on: https://gerrit.mot.com/1437667
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -36,6 +36,7 @@ enum {
 	Opt_gid_derivation,
 	Opt_default_normal,
 	Opt_unshared_obb,
+	Opt_nocache,
 	Opt_err,
 };
 
@@ -51,6 +52,7 @@ static const match_table_t sdcardfs_tokens = {
 	{Opt_default_normal, "default_normal"},
 	{Opt_unshared_obb, "unshared_obb"},
 	{Opt_reserved_mb, "reserved_mb=%u"},
+	{Opt_nocache, "nocache"},
 	{Opt_err, NULL}
 };
 
@@ -74,6 +76,7 @@ static int parse_options(struct super_block *sb, char *options, int silent,
 	/* by default, gid derivation is off */
 	opts->gid_derivation = false;
 	opts->default_normal = false;
+	opts->nocache = false;
 
 	*debug = 0;
 
@@ -133,6 +136,9 @@ static int parse_options(struct super_block *sb, char *options, int silent,
 			break;
 		case Opt_unshared_obb:
 			opts->unshared_obb = true;
+			break;
+		case Opt_nocache:
+			opts->nocache = true;
 			break;
 		/* unknown option */
 		default:
